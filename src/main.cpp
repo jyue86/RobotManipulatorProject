5c6d5f04b92417b5dfe7b575d61d50e999dd7cec
An Dang <dangthanhan507@gmail.com>
2023-07-04 23:02:47 -0700

    Saving progress. We are able to control the arm, but the brick falls off

--- src/main.cpp
+++ src/main.cpp
@@ -29,6 +29,7 @@
 #include <drake/geometry/drake_visualizer.h>
 
 #include <drake/manipulation/kuka_iiwa/iiwa_constants.h>
+#include <drake/manipulation/schunk_wsg/schunk_wsg_position_controller.h>
 
 #include <iostream>
 
@@ -58,6 +59,8 @@ int runMain()
 {
     systems::DiagramBuilder<double> builder;
     auto [plant, scene_graph] = multibody::AddMultibodyPlantSceneGraph(&builder, MULTIBODY_DT);
+    multibody::MultibodyPlant<double> plant_arm(MULTIBODY_DT);
+
     const std::string urdf = FindResourceOrThrow(ARM_PATH);
     const std::string gripper_sdf = FindResourceOrThrow(GRIPPER_PATH);
     const std::string brick_sdf = FindResourceOrThrow(BRICK_PATH);
@@ -70,35 +73,48 @@ int runMain()
     auto brick_instance =   parser.AddModels(brick_sdf).at(0);
     
     plant.WeldFrames(plant.world_frame(), plant.GetFrameByName("base"));
-    plant.WeldFrames(plant.GetFrameByName("iiwa_link_7"), plant.GetFrameByName("body"));
+    plant.WeldFrames(plant.GetFrameByName("iiwa_link_ee"), plant.GetFrameByName("body") );
     plant.Finalize();
+
+
+    multibody::Parser parser_arm(&plant_arm);
+    parser_arm.AddModels(urdf).at(0);
+    plant_arm.WeldFrames(plant_arm.world_frame(), plant_arm.GetFrameByName("base"));
+    plant_arm.Finalize();
 
     const int num_joints = plant.num_positions(); //9 actuators total (7 iiwa, 2 gripper) .... (this returns 16 positions for brick)
 
 
     const int num_joints_iiwa = 7;
     //shitty pid gains
-    Eigen::VectorXd kp = Eigen::VectorXd::Zero(num_joints);
-    Eigen::VectorXd ki = Eigen::VectorXd::Zero(num_joints);
-    Eigen::VectorXd kd = Eigen::VectorXd::Zero(num_joints);
+    Eigen::VectorXd kp = Eigen::VectorXd::Zero(kIiwaArmNumJoints);
+    Eigen::VectorXd ki = Eigen::VectorXd::Zero(kIiwaArmNumJoints);
+    Eigen::VectorXd kd = Eigen::VectorXd::Zero(kIiwaArmNumJoints);
     kp.fill(0.3); //PD controller
-    auto controller = builder.AddSystem<systems::controllers::InverseDynamicsController<double>>(plant,kp,ki,kd,false);
+    auto controller = builder.AddSystem<systems::controllers::InverseDynamicsController<double>>(plant_arm,kp,ki,kd,false);
 
-    drake::VectorX<double> q_des(num_joints);
+    drake::VectorX<double> q_des(kIiwaArmNumJoints);
     q_des.fill(1);
-    drake::VectorX<double> v_des(num_joints);
+    drake::VectorX<double> v_des(kIiwaArmNumJoints);
     v_des.fill(0.0);
-    drake::VectorX<double> desired_state_input(2 * num_joints);
+    drake::VectorX<double> desired_state_input(2 * kIiwaArmNumJoints);
     desired_state_input << q_des, v_des;
+
+    drake::VectorX<double> zero_act_schunk(2);
+    zero_act_schunk << 0, 0;
 
     auto desired_state_source = builder.AddSystem<systems::ConstantVectorSource<double>>(desired_state_input);
     desired_state_source->set_name("desired_state_constant");
 
+    auto act_schunk = builder.AddSystem<systems::ConstantVectorSource<double>>(zero_act_schunk);
+    act_schunk->set_name("act_schunk");
+
     //connect only arm/iiwa
-    builder.Connect(plant.get_state_output_port(),controller->get_input_port_estimated_state());
-    builder.Connect(controller->get_output_port_control(),plant.get_actuation_input_port());
+    builder.Connect(plant.get_state_output_port(arm_instance),controller->get_input_port_estimated_state());
+    builder.Connect(controller->get_output_port_control(),plant.get_actuation_input_port(arm_instance));
 
     builder.Connect(desired_state_source->get_output_port(), controller->get_input_port_desired_state());
+    builder.Connect(act_schunk->get_output_port(), plant.get_actuation_input_port(gripper_instance));
 
     //start visual + simulation
     geometry::DrakeVisualizerd::AddToBuilder(&builder, scene_graph);
